636215ec548308b7b39f3e345835bfdae94faa08
QuangPM - VFA <quangpm220503vt@gmail.com>
2023-12-03 20:42:53 +0700

    feat: fix bug

--- dist/assets/DeliveredDetailOrderComponent-4c42e30e.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-3f1fec90.js";const t=()=>e.jsx("div",{children:"Delivered Detail Order Component"});export{t as default};
---
+++ dist/assets/NotificationDetailOrderComponent-19f35524.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-36f80f1e.js";const i=()=>t.jsx("div",{children:"Notification Detail Order Component"});export{i as default};
