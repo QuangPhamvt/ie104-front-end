41ab0f507c12ab55c72b223e413362ff8f70848a
QuangPM - VFA <quangpm220503vt@gmail.com>
2023-12-04 09:40:02 +0700

    fix: i18next

--- dist/assets/NotificationDetailOrderComponent-1296366f.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-20320ee5.js";const i=()=>t.jsx("div",{children:"Notification Detail Order Component"});export{i as default};
--- dist/assets/ProcessingDetailOrderComponent-83f32d86.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-20320ee5.js";const o=()=>e.jsx("div",{children:"Processing Detail Order Component"});export{o as default};
